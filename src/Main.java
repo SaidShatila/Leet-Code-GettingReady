11fcc7f278320a3345f1be276a9fac2b9b2c644b
Said <said.shatila@totersapp.com>
2024-10-21 21:15:03 +0300

    finished two questions

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,37 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 6, 5, 3};
         int target = 11;
-        String [] strs = {"act","pots","tops","cat","stop","hat"};
+        String[] strs = {"act", "pots", "tops", "cat", "stop", "hat"};
         String stringOne = "racecar";
         String stringTwo = "carrace";
+
+        int[] nums1 = {1, 2, 2, 3, 3, 3};
+        int k1 = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums1, k1))); // Replace 'yourMethodName' with your function name
+
+        // Example 2
+        int[] nums2 = {1, 1, 1, 2, 2, 3};
+        int k2 = 1;
+        System.out.println(Arrays.toString(topKFrequent(nums2, k2)));
+
+        // Example 3
+        int[] nums3 = {7, 7, 8, 8, 8, 9};
+        int k3 = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums3, k3)));
+
+        int[] nums4 = {1, 2, 2, 3, 3, 3};
+        int k4 = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums4, k4)));
+
+        int[] nums5 = {1, 2};
+        int k5 = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums5, k5)));
+
+        List<String> strs1 = Arrays.asList("hello", "world", "");
+        String encoded = encode(strs1);
+        System.out.println("Encoded: " + encoded);
+        List<String> decoded = decode(encoded);
+        System.out.println("Decoded: " + decoded);
 
         System.out.println(isAnagram(stringOne, stringTwo));
         System.out.println(Arrays.toString(twoSum(nums, target)));
@@ -62,7 +90,7 @@ public class Main {
 
             // If the complement exists in the map, return both indices
             if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), index };
+                return new int[]{map.get(complement), index};
             }
 
             // Store the current value and its index
@@ -77,30 +105,73 @@ public class Main {
     public static List<List<String>> groupAnagrams(String[] strs) {
         String[] sortedStrs = new String[strs.length];
         List<List<String>> finalList = new ArrayList<>(Collections.emptyList());
-      for( int i = 0 ; i < strs.length ; i++ ){
+        for (int i = 0; i < strs.length; i++) {
             char[] chars = strs[i].toCharArray();
             Arrays.sort(chars);
             String sortedStr = new String(chars);
             sortedStrs[i] = sortedStr;
         }
 
-      HashMap<String ,List<String>> mapOfAnagrams = new HashMap<>();
+        HashMap<String, List<String>> mapOfAnagrams = new HashMap<>();
 
-        for( int i = 0 ; i < strs.length ; i++ ){
-            if (mapOfAnagrams.containsKey(sortedStrs[i])){
+        for (int i = 0; i < strs.length; i++) {
+            if (mapOfAnagrams.containsKey(sortedStrs[i])) {
                 mapOfAnagrams.get(sortedStrs[i]).add(strs[i]);
             }
 
-          if (!mapOfAnagrams.containsKey(sortedStrs[i])){
-              mapOfAnagrams.put(sortedStrs[i], new ArrayList<>());
-              mapOfAnagrams.get(sortedStrs[i]).add(strs[i]);
-          }
+            if (!mapOfAnagrams.containsKey(sortedStrs[i])) {
+                mapOfAnagrams.put(sortedStrs[i], new ArrayList<>());
+                mapOfAnagrams.get(sortedStrs[i]).add(strs[i]);
+            }
 
-      }
+        }
         finalList.addAll(mapOfAnagrams.values());
 
 
         return finalList;
     }
 
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+        List<int[]> values = new ArrayList<>();  // Initialize list to store the keys\
+
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            values.add(new int[]{entry.getValue(), entry.getKey()});
+        }
+
+        values.sort((a, b) -> b[0] - a[0]);
+
+        int[] topKElements = new int[k]; // Initialize an array to store the top k elements.
+
+        for (int i = 0; i < k; i++) {
+            topKElements[i] = values.get(i)[1]; // Get the key (which is at index 1 of the int[] array).
+        }
+
+        return topKElements;
+    }
+
+    public static String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for (String str : strs) {
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
+    }
+
+    public static List<String> decode(String str) {
+        int i = 0;
+        List<String> decoded = new ArrayList<>();
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1 + length;
+            decoded.add(str.substring(j + 1, i));
+        }
+        return decoded;
+    }
 }
